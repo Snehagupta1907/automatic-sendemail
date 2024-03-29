7e657b5b7db07b585a9f7b65a2547e1f831e6490
Snehagupta1907 <snehagupta98930@gmail.com>
2024-03-29 16:28:55 +0530

    change

--- routes/route.js
+++ routes/route.js
@@ -2,6 +2,10 @@ import express from 'express';
 import { sendEmailController } from '../controllers/sendEmailController.js';
 const router=express.Router();
 
+
+router.get('/test',(req,res)=>{
+    res.send('Hello World');
+})
 router.post('/send-email',sendEmailController);
 
 export default router;
